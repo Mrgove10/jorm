806e72546ece940ee9d41f6df8e609620225e5ba
Unknown <wolfla@hotmail.fr>
2019-11-13 19:59:47 +0100

    add function to get the parameters on entry

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,22 +1,48 @@
+import classes.Choice;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class main {
-    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
     public static void main(String[] args) throws IOException {
-        System.out.println("Press 1 to import a file");
-        System.out.println("Press 2 to export from database");
-        int choice = Integer.parseInt(reader.readLine());
-        switch (choice){
+
+        Choice choice = GetParameters();
+
+        switch (choice.TypeChoice){
             case 1:
-                System.out.println("upload to db");
+                System.out.println("Import in db...");
                 break;
             case 2:
-                System.out.println("Export from db");
+                System.out.println("Export from db...");
                 break;
             default:
-                System.out.println("wrong option selected");
+                System.out.println("Wrong option selected");
         }
+    }
+
+    public static Choice GetParameters() throws IOException {
+        Choice choiceAction = new Choice();
+
+        System.out.println("Press 1 to import a file");
+        System.out.println("Press 2 to export from database");
+        int type = Integer.parseInt(reader.readLine());
+
+        System.out.println("Root of the file export/import:");
+        String rootFile = reader.readLine();
+
+        System.out.println("URL JDBC :");
+        String jdbcUrl = reader.readLine();
+
+        System.out.println("Root of the logs file :");
+        String logsFile = reader.readLine();
+
+        choiceAction.TypeChoice = type;
+        choiceAction.RootFile = rootFile;
+        choiceAction.JdbcUrl = jdbcUrl;
+        choiceAction.RootLogsFile = logsFile;
+
+        return choiceAction;
     }
 }
