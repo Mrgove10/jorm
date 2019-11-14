002d493d2bac0d8c39be157b0bb70bd97316ad17
Adrien <richardadrien0@gmail.com>
2019-11-14 11:27:33 +0100

    Delete db_config.java

--- src/main/java/config/db_config.java
+++
@@ -1,7 +0,0 @@
-package config;
-
-public class db_config {
-    public String url = "jdbc:mysql://stephan-server-xl.duckdns.org:3306/TestUser?useSSL=false";
-    public String user = "TestUser";
-    public String mdp = "X5m^@g8mXqrj";
-}
