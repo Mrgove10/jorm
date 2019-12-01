36fbbadf37549fac3f3f097570f0cdc93c2e07f2
Adrien RICHARD <richardadrien0@gmail.com>
2019-12-02 00:49:38 +0100

    update sql

--- database.sql
+++ database.sql
@@ -13,7 +13,7 @@ create table LiveAlbum (
     AlbumID int null,
     PlaceOfRecording varchar(2048) null,
     constraint liveAlbum_ID_uindex unique (ID),
-    constraint liveAlbum_creation_ID_fk foreign key (AlbumID) references Album (ID)
+    constraint liveAlbum_creation_ID_fk foreign key (AlbumID) references Album (ID) on update cascade on delete cascade
   ) comment 'Table liveAlbum';
 alter table LiveAlbum
 add
@@ -23,7 +23,7 @@ create table BOAlbum (
     AlbumID int null,
     Film varchar(2048) null,
     constraint boAlbum_ID_uindex unique (ID),
-    constraint boAlbum_creation_ID_fk foreign key (AlbumID) references Album (ID)
+    constraint boAlbum_creation_ID_fk foreign key (AlbumID) references Album (ID) on update cascade on delete cascade
   ) comment 'Table BOAlbum';
 alter table BOAlbum
 add
@@ -32,136 +32,136 @@ add
 INSERT INTO `Album` (`ID`, `Members`, `Title`, `DateRelease`)
 VALUES
   (
-    NULL,
-    'Nathan Gaume',
-    'Out Of Tomorrow',
-    '2019-11-05'
+    1,
+    "Nathan Gaume",
+    "Out Of Tomorrow",
+    "2019-11-05"
   ),
   (
-    NULL,
-    'Fabrice Lemoine,Colin Carpentier',
+    2,
+    "Fabrice Lemoine,Colin Carpentier",
     "Honey, We're Crazy In Love",
-    '2019-10-10'
+    "2019-10-10"
   ),
   (
-    NULL,
-    'Damien Lemaître,Alexandra Lozé',
-    'Dance With Me',
-    '2019-11-14'
+    3,
+    "Damien Lemaître,Alexandra Lozé",
+    "Dance With Me",
+    "2019-11-14"
   ),
   (
-    NULL,
-    'Clarisse Allaire',
-    'She knows You Miss Me',
-    '2019-11-24'
+    4,
+    "Clarisse Allaire",
+    "She knows You Miss Me",
+    "2019-11-24"
   ),
   (
-    NULL,
-    'Joséphine Vannier,Gwenaël Dutertre',
+    5,
+    "Joséphine Vannier,Gwenaël Dutertre",
     "I think I'm In Love With You",
-    '2018-12-14'
+    "2018-12-14"
   ),
   (
-    NULL,
-    'Kévin Thiers,Kévin Thiers,Rebecca Messier',
-    'Do You Remember?',
-    '2019-09-05'
+    6,
+    "Kévin Thiers,Kévin Thiers,Rebecca Messier",
+    "Do You Remember?",
+    "2019-09-05"
   ),
   (
-    NULL,
-    'Élisabeth Barbeau,Carine Rochette,Alexis Alarie',
-    'Golden Tomorrow',
-    '2019-11-13'
+    7,
+    "Élisabeth Barbeau,Carine Rochette,Alexis Alarie",
+    "Golden Tomorrow",
+    "2019-11-13"
   ),
   (
-    NULL,
-    'Leslie Sardou',
-    'She Loves She Knows',
-    '2019-11-02'
+    8,
+    "Leslie Sardou",
+    "She Loves She Knows",
+    "2019-11-02"
   ),
   (
-    NULL,
-    'Émilien Cochet,Roméo Périer',
-    'Remember The Times',
-    '2019-10-08'
+    9,
+    "Émilien Cochet,Roméo Périer",
+    "Remember The Times",
+    "2019-10-08"
   ),
   (
-    NULL,
-    'Samuel Guillaume,Claudia Masson',
-    'Dance With My Voice',
-    '2019-12-09'
+    10,
+    "Samuel Guillaume,Claudia Masson",
+    "Dance With My Voice",
+    "2019-12-09"
   ),
   (
-    NULL,
-    'Jacques Bougie',
-    'Images Of Fireworks',
-    '2019-10-05'
+    11,
+    "Jacques Bougie",
+    "Images Of Fireworks",
+    "2019-10-05"
   ),
   (
-    NULL,
-    'Clémence Chopin',
-    'Gift Of A Dream',
-    '2019-10-19'
+    12,
+    "Clémence Chopin",
+    "Gift Of A Dream",
+    "2019-10-19"
   ),
   (
-    NULL,
-    'Gabriel Robillard,Axel Delaunay',
-    'Male Power',
-    '2019-10-30'
+    13,
+    "Gabriel Robillard,Axel Delaunay",
+    "Male Power",
+    "2019-10-30"
   ),
   (
-    NULL,
-    'Bastien Celice,Diane Veil',
-    'Need Your Music',
-    '2019-11-07'
+    14,
+    "Bastien Celice,Diane Veil",
+    "Need Your Music",
+    "2019-11-07"
   ),
   (
-    NULL,
-    'Anna Arsenault',
-    'Heart Of The Morning',
-    '2019-11-26'
+    15,
+    "Anna Arsenault",
+    "Heart Of The Morning",
+    "2019-11-26"
   ),
   (
-    NULL,
-    'Magali Jégou,Justine Girardot',
-    'I think I Know',
-    '2019-10-27'
+    16,
+    "Magali Jégou,Justine Girardot",
+    "I think I Know",
+    "2019-10-27"
   ),
   (
-    NULL,
-    'Jacques Bougie,Lucie Delisle',
-    'Light Kisses',
-    '2019-09-03'
+    17,
+    "Jacques Bougie,Lucie Delisle",
+    "Light Kisses",
+    "2019-09-03"
   ),
   (
-    NULL,
-    'Jonathan Grandjean,Natanaël Loupe,Armelle Bernier',
-    'Last Games',
-    '2020-01-10'
+    18,
+    "Jonathan Grandjean,Natanaël Loupe,Armelle Bernier",
+    "Last Games",
+    "2020-01-10"
   ),
   (
-    NULL,
-    'Nathan Rapace,Lara Calvet',
-    'She knows She Wants You',
-    '2019-09-20'
+    19,
+    "Nathan Rapace,Lara Calvet",
+    "She knows She Wants You",
+    "2019-09-20"
   ),
   (
-    NULL,
-    'Théodore Périer',
-    'I think She Knows',
-    '2019-11-06'
+    20,
+    "Théodore Périer",
+    "I think She Knows",
+    "2019-11-06"
   );
 INSERT INTO `BOAlbum` (`ID`, `AlbumID`, `Film`)
 VALUES
-  (NULL, '7', 'La promenade des égoïstes'),
-  (NULL, '17', "Le masque d'un dictateur"),
-  (NULL, '10', 'La taverne des lucioles'),
-  (NULL, '13', 'La sensualité des îles'),
-  (NULL, '8', 'La solitude des anges');
+  (1, "7", "La promenade des égoïstes"),
+  (2, "17", "Le masque d'un dictateur "),
+  (3, " 10 ", " La taverne des lucioles "),
+  (4, " 13 ", " La sensualité des îles "),
+  (5, " 8 ", " La solitude des anges ");
 INSERT INTO `LiveAlbum` (`ID`, `AlbumID`, `PlaceOfRecording`)
 VALUES
-  (NULL, '9', 'Snowbush'),
-  (NULL, '22', 'Nearon '),
-  (NULL, '26', 'Aempleforth'),
-  (NULL, '15', 'Dalelry'),
-  (NULL, '11', 'Leeside');
+  (1, " 9 ", " Snowbush "),
+  (2, " 20 ", " Nearon "),
+  (3, " 6 ", " Aempleforth "),
+  (4, " 15 ", " Dalelry "),
+  (5, " 11 ", " Leeside ");
